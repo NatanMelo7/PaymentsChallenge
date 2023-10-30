82cf0f2fa969304617031361962950018eff8d3e
Natan Melo <natan01melo@hotmail.com>
2023-10-29 21:13:24 -0300

    First Commit

---
+++ src/main/java/com/br/PaymentsChallenge/PaymentsChallenge/PaymentsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.br.PaymentsChallenge.PaymentsChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/PaymentsChallenge/PaymentsChallenge/PaymentsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.PaymentsChallenge.PaymentsChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
